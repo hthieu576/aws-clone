1353404473c82e87be6265685d527118e5348130
Hoang Trung Hieu <hoangtrunghieu576@gmail.com>
2017-02-05 11:31:01 +0700

    create landing page

--- aws/config/initializers/secret_token.rb
+++ aws/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Aws::Application.config.secret_token = '2c355f9f3bbe8c6356c417367dec1d80c527e0a2ba4d0ec18689d653f0e6005ccf818c1763d52cd43376f4a24a775768558ebebb787d88546e33419b5c2c5035'
+Aws::Application.config.secret_token = '5491b87012e301cebca191a8479f470a95bc7f22785b772a42369cc10345d6be0249ccb130e279f085c69c09dad182726937ac171dec888a3a9269648b02a835'
